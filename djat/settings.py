6e1cc79f59b11b8df0073075bc69244bbae86ebc
Nikolay Berchev <ber4ev@abv.bg>
2021-01-13 22:51:14 +0200

    Switched to custom account logging in

--- djat/settings.py
+++ djat/settings.py
@@ -92,6 +92,13 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'accounts.Account'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.AllowAllUsersModelBackend',
+    'accounts.backends.CaseInsensitiveModelBackend',
+)
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
