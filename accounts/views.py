33837fa478cbab00ebdfd316b30061e6b10ac1cf
Nikolay Berchev <ber4ev@abv.bg>
2021-01-14 21:10:19 +0200

    Added register feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+from .forms import RegistrationFrom
+
+
+def register_user(request, *args, **kwargs):
+    user = request.user
+
+    if user.is_authenticated:
+        return HttpResponse(f'You are already authenticated as {user.email}.')
+
+    context = {}
+
+    if request.POST:
+        form = RegistrationFrom(request.POST)
+
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email').lower()
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            destination = kwargs.get('next')
+
+            if destination:
+                return redirect(destination)
+            return redirect('home')
+        else:
+            context['registration_form'] = form
+
+    return render(request, 'accounts/register.html', context)
--- djat/urls.py
+++ djat/urls.py
@@ -5,7 +5,10 @@ from django.urls import path, include
 
 from personal.views import home_screen_view
 
+from accounts.views import register_user
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name='home'),
+    path('register/', register_user, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
