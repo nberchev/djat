cafff6d0a5bbae065f723021c205f8e089a87498
Nikolay Berchev <ber4ev@abv.bg>
2021-01-15 23:02:22 +0200

    Added login/logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 
-from .forms import RegistrationFrom
+from .forms import RegistrationFrom, AccountAuthenticationForm
 
 
 def register_user(request, *args, **kwargs):
@@ -22,7 +22,7 @@ def register_user(request, *args, **kwargs):
             raw_password = form.cleaned_data.get('password1')
             account = authenticate(email=email, password=raw_password)
             login(request, account)
-            destination = kwargs.get('next')
+            destination = get_redirect_if_exists(request)
 
             if destination:
                 return redirect(destination)
@@ -31,3 +31,43 @@ def register_user(request, *args, **kwargs):
             context['registration_form'] = form
 
     return render(request, 'accounts/register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
+def login_user(request, *args, **kwargs):
+    context = {}
+
+    user = request.user
+
+    if user.is_authenticated:
+        return redirect('home')
+
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                destination = get_redirect_if_exists(request)
+                if destination:
+                    return redirect(destination)
+                return redirect('home')
+        else:
+            context['login_form'] = form
+    return render(request, 'accounts/login.html', context)
+
+
+def get_redirect_if_exists(request):
+    redirect_page = None
+
+    if request.GET:
+        if request.GET.get('next'):
+            redirect_page = str(request.GET.get('next'))
+
+    return redirect_page
--- djat/urls.py
+++ djat/urls.py
@@ -5,10 +5,12 @@ from django.urls import path, include
 
 from personal.views import home_screen_view
 
-from accounts.views import register_user
+from accounts.views import register_user, login_user, logout_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name='home'),
     path('register/', register_user, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
